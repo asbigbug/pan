9230f2371da216d0f15d8b504a8758985fef7a55
psbHub <1796259615@qq.com>
2022-11-12 16:47:32 +0800

    11.12

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-
+import './static/style.scss'
 
 createApp(App).use(router).mount('#app')
--- src/view/login/index.vue
+++ src/view/login/index.vue
@@ -1,41 +1,96 @@
 <template>
-  <div class="wrap">
-    <h1 @click="goHome">login</h1>
-    <h1>{{ data.name }}</h1>
+  <div class="login">
+    <div class="getinfo">
+      <a-form :model="formState">
+        <a-form-item>
+          <a-input v-model:value="formState.userName" placeholder="用户名">
+            <template #prefix>
+              <user-outlined type="user" />
+            </template>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input v-model:value="formState.password" placeholder="密码">
+            <template #prefix>
+              <EyeInvisibleOutlined />
+            </template>
+          </a-input>
+        </a-form-item>
+      </a-form>
+      <div class="btn">
+        <a-button type="primary" @click="login" :loading="setBtn.loading">{{
+          setBtn.text
+        }}</a-button>
+        <br />
+        <a-button type="link">忘记密码</a-button>
+        <a-button type="link">注册</a-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { getUserinfo } from "@/request/requestAPI.js";
 import { reactive, ref } from "@vue/reactivity";
 import { useRouter } from "vue-router";
-const router = useRouter();
-
-// const data = ref({});
-const data = reactive({ name: "", age: "" });
-// getUserinfo({ userName: "1796259615", passwrod: 3254 }).then((res) => {
-//   // data.value = res.data
-//   //  data = res.data
-//  Object.assign(data, res.data)
-// });
- function ds() {
-  const res =   getUserinfo({ userName: "1796259615", passwrod: 3254 })
-  console.log(res);
-  Object.assign(data,res.data)
-}
-  
-const goHome = () => {
-  router.push({ name: "Home" });
+import { UserOutlined, EyeInvisibleOutlined } from "@ant-design/icons-vue";
+import { message } from "ant-design-vue";
+import { getUserinfo } from "@/request/requestAPI";
+const ROUTER = useRouter();
+//  登录信息
+let formState = reactive({
+  userName: "",
+  password: "",
+});
+// 登录按钮配置
+let setBtn = reactive({
+  loading: false,
+  text: "登录",
+});
+//  登录按钮
+const login = () => {
+  if (!formState.userName && !formState.password) {
+    message.error("用户名或密码不能为空");
+    return;
+  }
+  setBtn.loading = true;
+  setBtn.text = "";
+  getUserinfo(formState).then((res) => {
+    if (res.code === 200) {
+      message.success(res.message);
+      ROUTER.push("Home");
+    } else {
+      setBtn.text = "登录";
+      setBtn.loading = false;
+    }
+    console.log(111);
+  });
 };
 </script>
 
 <style lang="scss">
-.wrap {
-  width: 500px;
-  height: 500px;
-  background: red;
-  h1 {
-    color: aqua;
+.login {
+  width: 100%;
+  height: 100%;
+  // background-color: rgb(142, 216, 142);
+  overflow: hidden;
+  background-image: url(../../static/image/login.webp);
+  background-size: 100% 100%;
+  .getinfo {
+    width: 70%;
+    margin: 0 auto;
+    margin-top: 45%;
+  }
+  .btn {
+    .ant-btn {
+      float: right;
+      margin-top: 20px;
+      &:nth-child(1) {
+        width: 50%;
+        font-weight: 700;
+        margin-right: 50%;
+        transform: translatex(50%);
+      }
+    }
   }
 }
 </style>
