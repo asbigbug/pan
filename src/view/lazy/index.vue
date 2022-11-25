943fc2039c15730ffa963e5f7ad79bab421ea85c
psbHub <1796259615@qq.com>
2022-11-26 01:57:11 +0800

    1zys

--- src/view/lazy/index.vue
+++ src/view/lazy/index.vue
@@ -7,6 +7,6 @@ import { useRouter } from "vue-router";
 const ROUTER = useRouter();
 if (!localStorage.getItem("token")) {
   ROUTER.push({ name: "Login" });
-//   console.log(?);
+//   console.log(1.56);
 }
 </script>
